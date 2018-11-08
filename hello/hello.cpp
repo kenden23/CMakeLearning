023466aa5a58699d1dc9bc4595d578ae18a6299f
BillSu <35948036@qq.com>
2018-11-08 14:14:14 +0800

    add hello.cpp

---
+++ hello/hello.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TutorialConfig.h"
+
+#ifdef USE_MYMATH
+#include "utils"
+#endif
+
+int main(int argc, char *argv[])
+{
+   printf("hello, world.\n");
+
+   fprintf(stdout, "%s Version %d.%d\n",
+	   argv[0],
+	   Tutorial_VERSION_MAJOR,
+	   Tutorial_VERSION_MINOR);
+   fprintf(stdout, "Usage: %s number\n", argv[0]);
+
+   int a = 6, b = 2, c = 6;
+#ifdef USE_MYMATH
+   c = add(a, b);
+#else
+   c = a + b;
+#endif
+
+   printf("a + b  = %d\n", c);
+
+   system("pause");
+
+   return 0;
+}
